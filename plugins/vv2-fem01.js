ae9cbff575914f5ecb230ec9f249d05daa524a28
𝑻𝑰𝑻𝑼𝑳𝑨𝑹 𝑱𝑶𝑺𝑿 <benjaminmijahuanga@gmail.com>
2024-08-05 14:22:04 -1100

    Update vv2-fem01.js

--- plugins/vv2-fem01.js
+++ plugins/vv2-fem01.js
@@ -18,7 +18,7 @@ let handler = async (m, { conn, usedPrefix, command, paypal }) => {
     await m.react('✅');
 }
 
-handler.help = ['vv2fem00'];
-handler.tags = ['vv2fem00'];
-handler.command = /^vv2fem00$/i;
+handler.help = ['vv2fem01'];
+handler.tags = ['vv2fem01'];
+handler.command = /^vv2fem01$/i;
 export default handler;
