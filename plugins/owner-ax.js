bd71bd22523155fdd1e6f04f47cabc9e3602d645
𝑻𝑰𝑻𝑼𝑳𝑨𝑹 𝑱𝑶𝑺𝑿 <benjaminmijahuanga@gmail.com>
2024-08-10 23:16:52 -1100

    Update owner-ax.js

--- plugins/owner-ax.js
+++ plugins/owner-ax.js
@@ -2,7 +2,7 @@ let handler = (m) => m;
 handler.all = async function (m) {
 
 
-  if (/^Benjamin|Benja|BENJA|BENJAMIN|Novio|Benja$/i.test(m.text)) {
+  if (/^Benjamin|Benja|BENJA|BENJAMIN|Novio|NovioDeAnto$/i.test(m.text)) {
     var ax = `𝙲𝚁𝙴𝙰𝙳𝙾𝚁 @${global.suittag} 𝚄𝙽 𝙷𝚄𝙼𝙰𝙽𝙾 𝚃𝙴 𝙰 𝙸𝙽𝚅𝙾𝙲𝙰𝙳𝙾`;
 
 
