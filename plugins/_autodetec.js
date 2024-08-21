de09d9b3a2b9c6a1efa08aa66ef7f71c2452f3b7
𝑻𝑰𝑻𝑼𝑳𝑨𝑹 𝑱𝑶𝑺𝑿 <benjaminmijahuanga@gmail.com>
2024-08-20 19:08:51 -1100

    Update _autodetec.js

--- plugins/_autodetec.js
+++ plugins/_autodetec.js
@@ -16,22 +16,22 @@ export async function before(m, {conn}) {
   if (m.messageStubType == 21) {
     await this.sendMessage(
       m.chat,
-      {text: `*\`╭━〔  𝐌𝐈𝐙𝐔𝐊𝐈 | 𝐁𝐎𝐓  〕━╮\`*\n┃➔ *🗣𝙐𝙨𝙪𝙖𝙧𝙞𝙤:* ${usuario}\n┃➔ *✅𝘿𝙚𝙩𝙚𝙘𝙩:* Cambió el nombre de este grupo.\n┃➔ *❗𝙉𝙪𝙚𝙫𝙤:* ${m.messageStubParameters[0]}\n╰━━━━━━━━━━━━━━━╯`, mentions: [m.sender]},
+      {text: `*\`╭━〔 ＳＹＳＴＥＭ Ｘ 〕━╮\`*\n┃➔ *🗣𝙐𝙨𝙪𝙖𝙧𝙞𝙤:* ${usuario}\n┃➔ *✅𝘿𝙚𝙩𝙚𝙘𝙩:* Cambió el nombre de este grupo.\n┃➔ *❗𝙉𝙪𝙚𝙫𝙤:* ${m.messageStubParameters[0]}\n╰━━━━━━━━━━━━━━━╯`, mentions: [m.sender]},
       {quoted: fkontak}
     );
   } else if (m.messageStubType == 22) {
-    await this.sendMessage(m.chat, {text: `*\`╭━〔  𝐌𝐈𝐙𝐔𝐊𝐈 | 𝐁𝐎𝐓  〕━╮\`*\n┃➔ *🗣𝙐𝙨𝙪𝙖𝙧𝙞𝙤:* ${usuario}\n┃➔ *✅𝘿𝙚𝙩𝙚𝙘𝙩:* Cambió la foto de este grupo.\n╰━━━━━━━━━━━━━━━╯`, mentions: [m.sender]}, {quoted: fkontak});
+    await this.sendMessage(m.chat, {text: `*\`╭━〔 ＳＹＳＴＥＭ Ｘ 〕━╮\`*\n┃➔ *🗣𝙐𝙨𝙪𝙖𝙧𝙞𝙤:* ${usuario}\n┃➔ *✅𝘿𝙚𝙩𝙚𝙘𝙩:* Cambió la foto de este grupo.\n╰━━━━━━━━━━━━━━━╯`, mentions: [m.sender]}, {quoted: fkontak});
   } else if (m.messageStubType == 24) {
     await this.sendMessage(
       m.chat,
-      {text: `*\`╭━〔  𝐌𝐈𝐙𝐔𝐊𝐈 | 𝐁𝐎𝐓  〕━╮\`*\n┃➔ *🗣𝙐𝙨𝙪𝙖𝙧𝙞𝙤:* ${usuario}\n┃➔ *✅𝘿𝙚𝙩𝙚𝙘𝙩:* Cambió la descripción de este grupo.\n┃➔ *❗𝙉𝙪𝙚𝙫𝙤:* ${m.messageStubParameters[0]}\n╰━━━━━━━━━━━━━━━╯`, mentions: [m.sender]},
+      {text: `*\`╭━〔 ＳＹＳＴＥＭ Ｘ 〕━╮\`*\n┃➔ *🗣𝙐𝙨𝙪𝙖𝙧𝙞𝙤:* ${usuario}\n┃➔ *✅𝘿𝙚𝙩𝙚𝙘𝙩:* Cambió la descripción de este grupo.\n┃➔ *❗𝙉𝙪𝙚𝙫𝙤:* ${m.messageStubParameters[0]}\n╰━━━━━━━━━━━━━━━╯`, mentions: [m.sender]},
       {quoted: fkontak}
     );
   } else if (m.messageStubType == 25) {
     await this.sendMessage(
       m.chat,
       {
-        text: `*\`╭━〔  𝐌𝐈𝐙𝐔𝐊𝐈 | 𝐁𝐎𝐓  〕━╮\`*\n┃➔ *✅𝘿𝙚𝙩𝙚𝙘𝙩:* Ahora ${m.messageStubParameters[0] == "on" ? "solo los administradores" : "todos los miembros"} de este grupo, pueden editar la información de este grupo.\n╰━━━━━━━━━━━━━━━╯`,
+        text: `*\`╭━〔 ＳＹＳＴＥＭ Ｘ 〕━╮\`*\n┃➔ *✅𝘿𝙚𝙩𝙚𝙘𝙩:* Ahora ${m.messageStubParameters[0] == "on" ? "solo los administradores" : "todos los miembros"} de este grupo, pueden editar la información de este grupo.\n╰━━━━━━━━━━━━━━━╯`,
         mentions: [m.sender],
       },
       {quoted: fkontak}
@@ -40,7 +40,7 @@ export async function before(m, {conn}) {
     await this.sendMessage(
       m.chat,
       {
-        text: `*\`╭━〔  𝐌𝐈𝐙𝐔𝐊𝐈 | 𝐁𝐎𝐓  〕━╮\`*\n┃➔ 🗣𝙐𝙨𝙪𝙖𝙧𝙞𝙤: ${usuario}\n┃➔ ✅𝘿𝙚𝙩𝙚𝙘𝙩: Grupo ${m.messageStubParameters[0] == "on" ? "Cerrado 🔒" : "Abierto 🔓"}\n┃➔ ❗𝘼𝙫𝙞𝙨𝙤: ${m.messageStubParameters[0] == "on" ? "Solo los administradores pueden escribir" : "Ya pueden escribir todos" } en este grupo.\n╰━━━━━━━━━━━━━━━╯`,
+        text: `*\`╭━〔 ＳＹＳＴＥＭ Ｘ 〕━╮\`*\n┃➔ 🗣𝙐𝙨𝙪𝙖𝙧𝙞𝙤: ${usuario}\n┃➔ ✅𝘿𝙚𝙩𝙚𝙘𝙩: Grupo ${m.messageStubParameters[0] == "on" ? "Cerrado 🔒" : "Abierto 🔓"}\n┃➔ ❗𝘼𝙫𝙞𝙨𝙤: ${m.messageStubParameters[0] == "on" ? "Solo los administradores pueden escribir" : "Ya pueden escribir todos" } en este grupo.\n╰━━━━━━━━━━━━━━━╯`,
         mentions: [m.sender],
       },
       {quoted: fkontak}
@@ -49,7 +49,7 @@ export async function before(m, {conn}) {
     await this.sendMessage(
       m.chat,
       {
-        text: `*\`╭━〔  𝐌𝐈𝐙𝐔𝐊𝐈 | 𝐁𝐎𝐓  〕━╮\`*\n┃➔ 🗣𝙐𝙨𝙪𝙖𝙧𝙞𝙤: @${m.messageStubParameters[0].split`@`[0]}\n┃➔ Un miembro se salió del grupo.\n┃➔ ¡LARGATE CTMR<3¡\n╰━━━━━━━━━━━━━━━╯`,
+        text: `*\`╭━〔 ＳＹＳＴＥＭ Ｘ 〕━╮\`*\n┃➔ 🗣𝙐𝙨𝙪𝙖𝙧𝙞𝙤: @${m.messageStubParameters[0].split`@`[0]}\n┃➔ Un miembro se salió del grupo.\n┃➔ ¡LARGATE CTMR<3¡\n╰━━━━━━━━━━━━━━━╯`,
         mentions: [`${m.sender}`, `${m.messageStubParameters[0]}`]
       },
       {quoted: fkontak}
@@ -58,7 +58,7 @@ export async function before(m, {conn}) {
     await this.sendMessage(
       m.chat,
       {
-        text: `*\`╭━〔 𝐒𝐘𝐒𝐓𝐄𝐌 𝐗 〕━╮\`*\n┃➔ 🗣𝙐𝙨𝙪𝙖𝙧𝙞𝙤: ${usuario}\n┃➔ ✅𝘿𝙚𝙩𝙚𝙘𝙩: Ascendió a un usuario con los poderes de administrador.\n┃➔ ❗𝘼𝙫𝙞𝙨𝙤: @${m.messageStubParameters[0].split`@`[0]} Se convirtió en admin de este grupo.\n╰━━━━━━━━━━━━━━━╯`,
+        text: `*\`╭━〔 ＳＹＳＴＥＭ Ｘ 〕━╮\`*\n┃➔ 🗣𝙐𝙨𝙪𝙖𝙧𝙞𝙤: ${usuario}\n┃➔ ✅𝘿𝙚𝙩𝙚𝙘𝙩: Ascendió a un usuario con los poderes de administrador.\n┃➔ ❗𝘼𝙫𝙞𝙨𝙤: @${m.messageStubParameters[0].split`@`[0]} Se convirtió en admin de este grupo.\n╰━━━━━━━━━━━━━━━╯`,
         mentions: [`${m.sender}`, `${m.messageStubParameters[0]}`],
       },
       {quoted: fkontak}
@@ -67,7 +67,7 @@ export async function before(m, {conn}) {
     await this.sendMessage(
       m.chat,
       {
-        text: `*\`╭━〔 𝐒𝐘𝐒𝐓𝐄𝐌 𝐗 〕━╮\`*\n┃➔ 🗣𝙐𝙨𝙪𝙖𝙧𝙞𝙤: ${usuario}\n┃➔ ✅𝘿𝙚𝙩𝙚𝙘𝙩: Descendió a un usuario con los poderes de administrador.\n┃➔ ❗𝘼𝙫𝙞𝙨𝙤: @${m.messageStubParameters[0].split`@`[0]} Se convirtió en miembro de este grupo.\n╰━━━━━━━━━━━━━━━╯`,
+        text: `*\`╭━〔 ＳＹＳＴＥＭ Ｘ 〕━╮\`*\n┃➔ 🗣𝙐𝙨𝙪𝙖𝙧𝙞𝙤: ${usuario}\n┃➔ ✅𝘿𝙚𝙩𝙚𝙘𝙩: Descendió a un usuario con los poderes de administrador.\n┃➔ ❗𝘼𝙫𝙞𝙨𝙤: @${m.messageStubParameters[0].split`@`[0]} Se convirtió en miembro de este grupo.\n╰━━━━━━━━━━━━━━━╯`,
         mentions: [`${m.sender}`, `${m.messageStubParameters[0]}`],
       },
       {quoted: fkontak}
@@ -75,11 +75,11 @@ export async function before(m, {conn}) {
   } else if (m.messageStubType == 72) {
     await this.sendMessage(
       m.chat,
-      {text: `*\`╭━〔  𝐌𝐈𝐙𝐔𝐊𝐈 | 𝐁𝐎𝐓  〕━╮\`*\n┃➔ 🗣𝙐𝙨𝙪𝙖𝙧𝙞𝙤: ${usuario}\n┃➔ ✅𝘿𝙚𝙩𝙚𝙘𝙩: Cambió la duración de los mensajes de este grupo.\n┃➔ ❗𝙉𝙪𝙚𝙫𝙤: @${m.messageStubParameters[0]}\n╰━━━━━━━━━━━━━━━╯`, mentions: [m.sender]},
+      {text: `*\`╭━〔 ＳＹＳＴＥＭ Ｘ 〕━╮\`*\n┃➔ 🗣𝙐𝙨𝙪𝙖𝙧𝙞𝙤: ${usuario}\n┃➔ ✅𝘿𝙚𝙩𝙚𝙘𝙩: Cambió la duración de los mensajes de este grupo.\n┃➔ ❗𝙉𝙪𝙚𝙫𝙤: @${m.messageStubParameters[0]}\n╰━━━━━━━━━━━━━━━╯`, mentions: [m.sender]},
       {quoted: fkontak}
     );
   } else if (m.messageStubType == 123) {
-    await this.sendMessage(m.chat, {text: `*\`╭━〔  𝐌𝐈𝐙𝐔𝐊𝐈 | 𝐁𝐎𝐓  〕━╮\`*\n┃➔ 🗣𝙐𝙨𝙪𝙖𝙧𝙞𝙤: ${usuario}\n┃➔ ✅𝘿𝙚𝙩𝙚𝙘𝙩: Desactivó los mensajes temporales de este grupo.\n╰━━━━━━━━━━━━━━━╯`, mentions: [m.sender]}, {quoted: fkontak});
+    await this.sendMessage(m.chat, {text: `*\`╭━〔 ＳＹＳＴＥＭ Ｘ 〕━╮\`*\n┃➔ 🗣𝙐𝙨𝙪𝙖𝙧𝙞𝙤: ${usuario}\n┃➔ ✅𝘿𝙚𝙩𝙚𝙘𝙩: Desactivó los mensajes temporales de este grupo.\n╰━━━━━━━━━━━━━━━╯`, mentions: [m.sender]}, {quoted: fkontak});
   } else {
     console.log({
       messageStubType: m.messageStubType,
