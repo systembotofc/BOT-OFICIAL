319346295b20ccfa80b848ed37fe136881a28a30
𝑻𝑰𝑻𝑼𝑳𝑨𝑹 𝑱𝑶𝑺𝑿 <benjaminmijahuanga@gmail.com>
2024-08-20 18:39:20 -1100

    Update propietario(a)-cleartmp.js

--- plugins/propietario(a)-cleartmp.js
+++ plugins/propietario(a)-cleartmp.js
@@ -21,7 +21,7 @@ unlinkSync(file)
 })} //NO USAR ESTE COMANDO EN HEROKU | DO NOT USE THIS COMMAND ON HEROKU
 handler.help = ['cleartmp']
 handler.tags = ['owner']
-handler.command = /^(cleartmp|cleartemp|borrartmp|eliminartmp|borrartemp|borrartemp)$/i
+handler.command = /^(tmpsystemx|cleartemp|borrartmp|eliminartmp|borrartemp|borrartemp)$/i
 handler.exp = 500
 handler.rowner = true
 export default handler
