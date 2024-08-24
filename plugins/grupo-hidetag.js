5c3388495ccabb3259edff289ca214ae5a779325
𝑻𝑰𝑻𝑼𝑳𝑨𝑹 𝑱𝑶𝑺𝑿 <benjaminmijahuanga@gmail.com>
2024-08-23 18:12:57 -1100

    Update grupo-hidetag.js

--- plugins/grupo-hidetag.js
+++ plugins/grupo-hidetag.js
@@ -1,32 +1,34 @@
-import MessageType from '@whiskeysockets/baileys'
 import { generateWAMessageFromContent } from '@whiskeysockets/baileys'
 
-let handler = async (m, { conn, text, participants }) => {
-  // Verifica si el mensaje contiene el texto 'notify'
-  if (m.text.toLowerCase().startsWith('notify ')) {
-    let users = participants.map(u => conn.decodeJid(u.id))
-    let args = m.text.split(' ').slice(1).join(' ')
-    let q = m.quoted ? m.quoted : m
-    let c = m.quoted ? m.quoted : m.msg
-    
-    const msg = conn.cMod(m.chat,
-      generateWAMessageFromContent(m.chat, {
-        [c.toJSON ? q.mtype : 'extendedTextMessage']: c.toJSON ? c.toJSON() : {
-          text: c || args
-        }
-      }, {
-        userJid: conn.user.id
-      }),
-      args || q.text, conn.user.jid, { mentions: users }
-    )
-    await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
-  }
+const handleNotification = async (message, { conn, text, participants }) => {
+  // Extrae los IDs de los participantes del chat y los decodifica
+  let userIds = participants.map(user => conn.decodeJid(user.id))
+  
+  // Determina si el mensaje es citado o no
+  let originalMessage = message.quoted ? message.quoted : message
+  let messageContent = message.quoted ? message.quoted : message.msg
+  
+  // Genera el mensaje que será enviado
+  const formattedMessage = conn.cMod(message.chat,
+    generateWAMessageFromContent(message.chat, {
+      [messageContent.toJSON ? originalMessage.mtype : 'extendedTextMessage']: messageContent.toJSON ? messageContent.toJSON() : {
+        text: messageContent || ''
+      }
+    }, {
+      userJid: conn.user.id
+    }),
+    text || originalMessage.text, conn.user.jid, { mentions: userIds }
+  )
+  
+  // Envía el mensaje formateado al chat
+  await conn.relayMessage(message.chat, formattedMessage.message, { messageId: formattedMessage.key.id })
 }
 
-handler.help = ['notify <mensaje>']
-handler.tags = ['grupo']
-handler.command = ['notify'] 
-handler.group = true
-handler.admin = true
+// Configuración del comando
+handleNotification.help = ['notify <message>']
+handleNotification.tags = ['group']
+handleNotification.command = ['notify', 'n']
+handleNotification.group = true
+handleNotification.admin = true
 
-export default handler
+export default handleNotification
