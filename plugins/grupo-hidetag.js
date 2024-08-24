224241b4f8acaa271cca968ec36a7c34bde21ec3
𝑻𝑰𝑻𝑼𝑳𝑨𝑹 𝑱𝑶𝑺𝑿 <benjaminmijahuanga@gmail.com>
2024-08-23 18:10:46 -1100

    Update grupo-hidetag.js

--- plugins/grupo-hidetag.js
+++ plugins/grupo-hidetag.js
@@ -2,21 +2,25 @@ import MessageType from '@whiskeysockets/baileys'
 import { generateWAMessageFromContent } from '@whiskeysockets/baileys'
 
 let handler = async (m, { conn, text, participants }) => {
-  let users = participants.map(u => conn.decodeJid(u.id))
-  let q = m.quoted ? m.quoted : m
-  let c = m.quoted ? m.quoted : m.msg
-  
-  const msg = conn.cMod(m.chat,
-    generateWAMessageFromContent(m.chat, {
-      [c.toJSON ? q.mtype : 'extendedTextMessage']: c.toJSON ? c.toJSON() : {
-        text: c || ''
-      }
-    }, {
-      userJid: conn.user.id
-    }),
-    text || q.text, conn.user.jid, { mentions: users }
-  )
-  await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
+  // Verifica si el mensaje contiene el texto 'notify'
+  if (m.text.toLowerCase().startsWith('notify ')) {
+    let users = participants.map(u => conn.decodeJid(u.id))
+    let args = m.text.split(' ').slice(1).join(' ')
+    let q = m.quoted ? m.quoted : m
+    let c = m.quoted ? m.quoted : m.msg
+    
+    const msg = conn.cMod(m.chat,
+      generateWAMessageFromContent(m.chat, {
+        [c.toJSON ? q.mtype : 'extendedTextMessage']: c.toJSON ? c.toJSON() : {
+          text: c || args
+        }
+      }, {
+        userJid: conn.user.id
+      }),
+      args || q.text, conn.user.jid, { mentions: users }
+    )
+    await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
+  }
 }
 
 handler.help = ['notify <mensaje>']
